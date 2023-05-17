6a7137a9a763e769c0299712b7c5e2a49d6526fb
siwonKH <kanghyunkim313@gmail.com>
2023-05-17 17:08:46 +0900

    Add tray icon feature

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,55 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+
+use tauri::{Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, CustomMenuItem};
+use tauri_plugin_positioner::{Position, WindowExt};
+
 fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let system_tray_menu = SystemTrayMenu::new().add_item(quit);
     tauri::Builder::default()
+        .plugin(tauri_plugin_positioner::init())
+        .system_tray(SystemTray::new().with_menu(system_tray_menu))
+        .on_system_tray_event(|app, event| {
+            tauri_plugin_positioner::on_tray_event(app, &event);
+            match event {
+                SystemTrayEvent::LeftClick {
+                    position: _,
+                    size: _,
+                    ..
+                } => {
+                    let window = app.get_window("main").unwrap();
+
+                    println!("{}", window.is_visible().unwrap());
+                    if window.is_visible().unwrap() {
+                        window.hide().unwrap();
+                    } else {
+                        let _ = window.move_window(Position::TrayCenter);
+                        window.show().unwrap();
+                        window.set_focus().unwrap();
+                    }
+                }
+                SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                    "quit" => {
+                        std::process::exit(0);
+                    }
+                    _ => {}
+                },
+                _ => {}
+            }
+        })
+        .on_window_event(|event| match event.event() {
+            tauri::WindowEvent::Focused(is_focused) => {
+                // detect click outside of the focused window and hide the app
+
+                println!("Out side click");
+                if !is_focused {
+                    event.window().hide().unwrap();
+                }
+            }
+            _ => {}
+        })
         .invoke_handler(tauri::generate_handler![
             connect_to_wifi,
             disconnect_from_wifi,
@@ -41,16 +88,3 @@ fn disconnect_from_wifi() -> Result<String, String> {
         Err("failed to disconnect from the Wi-Fi network".to_string())
     }
 }
-
-//// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-//#[tauri::command]
-//fn greet(name: &str) -> String {
-//    format!("Hello, {}! You've been greeted from Rust!", name)
-//}
-//
-//fn main() {
-//    tauri::Builder::default()
-//        .invoke_handler(tauri::generate_handler![greet])
-//        .run(tauri::generate_context!())
-//        .expect("error while running tauri application");
-//}
